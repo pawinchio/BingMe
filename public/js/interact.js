9453fc588a6d2a48e2388402e1e3b72f013726dc
Boy <winnynow@hotmail.com>
2018-11-13 22:45:33 +0700

    add Menu by Boy

--- public/js/interact.js
+++ public/js/interact.js
@@ -1,8 +1,9 @@
 let interactPipe = undefined;
-let orderId = 'thisIsOrderId';
+let orderId = undefined;
 let user = undefined;
 
-const awakeInteractBoard = () => {
+const awakeInteractBoard = (e) => {
+    let placeData = e.target.parentNode.getAttribute('data-place-detail');
     let interactBoard = $('#interactBoard');
     interactBoard.css({
         "bottom":"0vh"
@@ -24,3 +25,21 @@ const killInteractBoard = () => {
     });
     if(interactPipe!=undefined)interactPipe.emit("disconnectRoom", orderId);
 }
+
+const addMenu = () =>{
+    //grabbing new food text from input
+    var Food = $("#inputFood").val();
+    $("#inputFood").val("");
+    //create a new li and add to ul
+    $("#showFood").append("<div class=d-flex justify-content-between><div class= 'mr-auto p-2' id=Food>"+Food +"</div><div class= 'p-2'><input class=countFood type=text ></div><div class= 'p-2' style='color:black;'>X</div></div>");
+    $(".countFood").val("1");
+}
+    
+
+$("#inputFood").keypress(function(event){
+	if(event.which === 13){
+		addMenu();
+	}
+});
+
+$("#addBot").on("click",addMenu);

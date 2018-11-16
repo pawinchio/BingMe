e7069ecb77028f69d705a010667bc6da3c316a3b
mark <pawinchio@yahoo.com>
2018-11-16 18:54:22 +0700

    install npm multer by pawin.c

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const   express = require('express'),
         LocalStrategy = require('passport-local'),
         passportLocalMongoose = require('passport-local-mongoose'),
         nodemailer = require('nodemailer'),
+        multer = require('multer'),
         uuid = require('uuid/v1');;
 
 var     Eater  = require("./models/eater"),
@@ -374,12 +375,35 @@ app.post('/eaterDataForm', (req,res) => {
                         console.log('Code Saving Failed'+err);
                 
                 });
-                console.log("save!!!!")  
-                
-
-               
-        
+                console.log("save!!!!")        
 });
-// search email in user Auth   ->>>  userDAta
+
+// ajax with jquery
+
+var storage =   multer.diskStorage({
+        destination: function (req, file, callback) {
+          callback(null, './uploads');
+        },
+        filename: function (req, file, callback) {
+          callback(null, file.fieldname + '-' + Date.now());
+        }
+      });
+
+      var upload = multer({ storage : storage}).single('userPhoto');
+      
+      app.get('/',function(req,res){
+            res.sendFile(__dirname + "/index.html");
+      });
+      
+      app.post('/api/photo',function(req,res){
+          upload(req,res,function(err) {
+              if(err) {
+                  return res.end("Error uploading file.");
+              }
+              res.end("File is uploaded");
+          });
+      });
+
+
 
 server.listen(5500, () => console.log('Server run on port 5500'));
